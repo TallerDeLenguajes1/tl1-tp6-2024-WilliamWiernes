61a7d46988751d0133c1c68e69de8cf716a1a018
“WilliamWiernes” <williambrandonwiernes@gmail.com>
2024-05-19 13:27:16 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -31,3 +31,208 @@ while (salida > 0)
 }
 
 Console.WriteLine("Número invertido: " + numInvertido); */
+
+/* // Ejercicio 2
+
+int continuar = 1, opcion;
+double suma, resta, multiplicacion, division;
+while (continuar == 1)
+{
+    opcion = Menu();
+
+    switch (opcion)
+    {
+        case 1:
+            suma = Suma();
+            Console.WriteLine("Resultado de la suma: " + suma);
+            break;
+        case 2:
+            resta = Resta();
+            Console.WriteLine("Resultado de la resta: " + resta);
+            break;
+        case 3:
+            multiplicacion = Multiplicacion();
+            Console.WriteLine("Resultado de la multiplicación: " + multiplicacion);
+            break;
+        case 4:
+            division = Division();
+            Console.WriteLine("Resultado de la división: " + division);
+            break;
+        default:
+            Console.WriteLine("Opcion no reconocida...");
+            break;
+    }
+
+    continuar = Continuar();
+}
+
+static int Menu()
+{
+    int opcion;
+    bool resul;
+
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    do
+    {
+        Console.WriteLine("Opcion: ");
+        string ingreso = Console.ReadLine();
+        resul = int.TryParse(ingreso, out opcion);
+    } while (!resul);
+
+    return opcion;
+}
+
+static double Suma()
+{
+    double num1, num2;
+    bool resul;
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 1: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num1);
+    } while (!resul);
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 2: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num2);
+    } while (!resul);
+
+    return num1 + num2;
+}
+
+static double Resta()
+{
+    double num1, num2;
+    bool resul;
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 1: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num1);
+    } while (!resul);
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 2: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num2);
+    } while (!resul);
+
+    return num1 - num2;
+}
+
+static double Multiplicacion()
+{
+    double num1, num2;
+    bool resul;
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 1: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num1);
+    } while (!resul);
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 2: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num2);
+    } while (!resul);
+
+    return num1 * num2;
+}
+
+static double Division()
+{
+    double num1, num2;
+    bool resul;
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 1: ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num1);
+    } while (!resul);
+
+    do
+    {
+        Console.WriteLine("Ingrese el número 2 (distinto de 0): ");
+        string ingreso = Console.ReadLine();
+        resul = double.TryParse(ingreso, out num2);
+    } while (!resul || num2 == 0);
+
+    return num1 / num2;
+}
+
+static int Continuar()
+{
+    int continuar;
+    bool resul;
+
+    do
+    {
+        Console.WriteLine("Desea realziar otra operación? [0] NO [1] SI");
+        string ingreso = Console.ReadLine();
+        resul = int.TryParse(ingreso, out continuar);
+    } while (!resul || (continuar != 0 && continuar != 1));
+
+    return continuar;
+} */
+
+// Ejercicio 3
+double num;
+bool resul;
+
+do
+{
+    Console.WriteLine("Ingrese un número decimal: ");
+    string ingreso = Console.ReadLine();
+    resul = double.TryParse(ingreso, out num);
+} while (!resul);
+
+Console.WriteLine("Valor absoluto: " + Math.Abs(num));
+Console.WriteLine("Cuadrado : " + Math.Pow(num, 2));
+Console.WriteLine("Raíz cuadrada: " + (num >= 0 ? Math.Sqrt(num).ToString() : "Número no válido"));
+Console.WriteLine("Seno: " + Math.Sin(num));
+Console.WriteLine("Coseno: " + Math.Cos(num));
+Console.WriteLine("Parte entera: " + Math.Truncate(num));
+
+int num1, num2;
+
+do
+{
+    Console.WriteLine("Ingrese el primer número: ");
+    string ingreso = Console.ReadLine();
+    resul = int.TryParse(ingreso, out num1);
+} while (!resul);
+
+do
+{
+    Console.WriteLine("Ingrese el segundo número: ");
+    string ingreso = Console.ReadLine();
+    resul = int.TryParse(ingreso, out num2);
+} while (!resul);
+
+if (num1 > num2)
+{
+    Console.WriteLine("Número 1 es el máximo");
+    Console.WriteLine("Número 2 es el mínimo");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine("Número 2 es el máximo");
+    Console.WriteLine("Número 1 es el mínimo");
+}
+else
+{
+    Console.WriteLine("Los números son iguales");
+}
